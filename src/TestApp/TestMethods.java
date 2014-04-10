4e6db6f27c63f67b993453d942ba1342c5ef0465
Matt Lyons <mattlyons0@gmail.com>
2014-04-09 23:37:16 -0500

    Started JUnit Implementation
    Got it working in a test scenerio. Now it needs to be implemented with arguments.

--- src/TestApp/TestMethods.java
+++ src/TestApp/TestMethods.java
@@ -4,7 +4,13 @@
  */
 package TestApp;
 
-import DropboxGrader.UnitTesting.JavaMethod;
+//import DropboxGrader.UnitTesting.JavaMethod;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.tools.ToolProvider;
 
 /**
  *
@@ -12,13 +18,36 @@ import DropboxGrader.UnitTesting.JavaMethod;
  */
 public class TestMethods {
     public static void main (String[] args) {
-        JavaMethod m=new JavaMethod("public static String toString(String[] args,   Object    o   ,String s,Object a){");
-        System.out.println(m);
-        JavaMethod m2=new JavaMethod("public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {");
-        System.out.println(m2);
+        testJUnit();
         
     }
-    static void test(String s){
+    public static String test(String s){
         s+="!";
+        return s;
+    }
+//    public static void testUnitTest(){
+//        JavaMethod m=new JavaMethod("public static String toString(String[] args,   Object    o   ,String s,Object a){");
+//        System.out.println(m);
+//        JavaMethod m2=new JavaMethod("public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {");
+//        System.out.println(m2);
+//    }
+    public static void testJUnit(){
+        String[] args={"java","-cp","/home/matt/Downloads/junit-4.11.jar:"
+                + "/home/matt/Downloads/hamcrest-core-1.3.jar:/home/matt/NetBeansProjects/Java-Grader/test/:"
+                + "/home/matt/NetBeansProjects/Java-Grader/build/classes/",
+                "org.junit.runner.JUnitCore","JUnitTest"};
+                ToolProvider.getSystemJavaCompiler().run(null, System.out, System.err,
+                        args[1],args[2],"/home/matt/NetBeansProjects/Java-Grader/test/"+args[4]+".java"
+                                ,"/home/matt/NetBeansProjects/Java-Grader/src/TestApp/TestMethods.java");
+        ProcessBuilder builder=new ProcessBuilder(args);
+        File dir=new File("/home/matt/NetBeansProjects/Java-Grader/test/");
+            builder.directory(dir);
+            builder.inheritIO();
+            System.out.println("Running from: "+dir+"\nArgs: "+Arrays.toString(args));
+        try {
+            builder.start();
+        } catch (IOException ex) {
+            Logger.getLogger(TestMethods.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
---
+++ test/JUnitTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import TestApp.TestMethods;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author matt
+ */
+public class JUnitTest {
+    
+    public JUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void testMethods(){
+        assertEquals("hello",TestMethods.test("hello"));
+    }
+}
