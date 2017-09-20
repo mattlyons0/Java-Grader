b28e1b26d48af030246d3ac3ed4d9578e7cf1c84
Matt Lyons <matt@mattlyons.net>
2017-09-19 22:54:24 -0700

    Add Mock Secrets File

---
+++ src/DropboxGrader/Data/Data.java
@@ -0,0 +1,16 @@
+package DropboxGrader.Data;
+
+/**
+ * This file is designed to contain secrets
+ * @author Matt Lyons
+ */
+public class Data {
+    //Dropbox API Data
+    public static final String APP_KEY = "";
+    public static final String APP_SECRET = "";
+    
+    //Mail Server Data
+    public static final String MAIL_SMTP_SERVER = "";
+    public static final String MAIL_USERNAME = "";
+    public static final String MAIL_PASS = "";
+}
